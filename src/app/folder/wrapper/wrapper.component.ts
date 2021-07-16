31c3bc443052cb762c3bb86a3dc5cd5b6b679dbc
Marco Montanari <sirmmo@gmail.com>
2021-07-16 20:15:25 +0200

    magic

--- src/app/folder/wrapper/wrapper.component.ts
+++ src/app/folder/wrapper/wrapper.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { APP_CONFIG } from '../../../environments/environment';
 
 @Component({
@@ -8,10 +10,19 @@ import { APP_CONFIG } from '../../../environments/environment';
 })
 export class WrapperComponent implements OnInit {
   title = APP_CONFIG.name;
-
-  constructor() { }
+  folder;
+  constructor(
+    private ar: ActivatedRoute,
+    private ht: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this.ar.params.subscribe(data=>{
+      this.ht.get('/assets/folders.json').subscribe((fldrs:any)=> {
+        this.folder = fldrs.folders.filter(x=>x.id === data.fid)[0];
+        console.log(this.folder);
+      })
+    })
   }
 
 }
