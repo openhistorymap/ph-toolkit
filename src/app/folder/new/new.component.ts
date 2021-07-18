f590c2c626da30e810945999a8c98fc83159599d
Marco Montanari <sirmmo@gmail.com>
2021-07-18 12:47:44 +0200

    update

--- src/app/folder/new/new.component.ts
+++ src/app/folder/new/new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { APP_CONFIG } from '../../../environments/environment';
+import { Octokit, App } from "octokit";
 
 @Component({
   selector: 'app-new',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewComponent implements OnInit {
 
+data = {
+  id: '8e060bd7-8291-4216-942f-3272ecf7cf71',
+  name:'Folder test',
+  slug:'folder_test',
+  private: false,
+  mode: 'single',
+
+}
+
+octokit: Octokit;
+
+  title = APP_CONFIG.name;
   constructor() { }
 
   ngOnInit(): void {
+    this.octokit = new Octokit();
+  }
+
+  
+
+  submitNew(){
+    this.octokit.rest.repos.createForAuthenticatedUser({
+      name: this.data.slug,
+      description: this.data.name,
+      has_issues: true,
+      has_wiki: true,
+      license_template: 'MIT',
+      private: this.data.private,
+      homepage: 'https://ph.openhistorymap.org/'+this.data.id
+    }).then(x=> {
+      console.log(x);
+    })
   }
 
 }
